f56c345bd33045680a034c5c505f42f3c2c1d9b2
Luis Cunha <luisfelipebc@outlook.com>
2022-07-20 15:22:55 -0300

    style pkg

--- R/gridbr_download.R
+++ R/gridbr_download.R
@@ -8,13 +8,18 @@
 #' @return IBGE statistical grid
 #' @export
 
-gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FALSE){
+gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FALSE) {
 
   # error: input class
-  if (!any(class(input) %in% c("sf", "sfc"))) stop('input must be of class simple features (sf)')
+  if (!any(class(input) %in% c("sf", "sfc"))) stop("input must be of class simple features (sf)")
 
   # message: internet connection
-  if (suppressWarnings(tryCatch({readLines("http://example.com/",n=1)}, error = function(e) FALSE)) == FALSE & "gridbr.data" %in% utils::installed.packages() == FALSE) {
+  if (suppressWarnings(tryCatch(
+    {
+      readLines("http://example.com/", n = 1)
+    },
+    error = function(e) FALSE
+  )) == FALSE & "gridbr.data" %in% utils::installed.packages() == FALSE) {
     message("couldn't connect to the internet. Population census data will not be loaded.")
     census_data <- FALSE
   }
@@ -41,9 +46,11 @@ gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FA
 
   # error: input coverage
   if (sf::st_bbox(input)[[1]] < 2800000 |
-      sf::st_bbox(input)[[2]] < 7350000 |
-      sf::st_bbox(input)[[3]] > 2800000+(10*500000) |
-      sf::st_bbox(input)[[4]] > 7350000+(10*500000)) stop('the input is outside IBGE Statistical Grid coverage.')
+    sf::st_bbox(input)[[2]] < 7350000 |
+    sf::st_bbox(input)[[3]] > 2800000 + (10 * 500000) |
+    sf::st_bbox(input)[[4]] > 7350000 + (10 * 500000)) {
+    stop("the input is outside IBGE Statistical Grid coverage.")
+  }
 
   if (cellsize == "200M") {
     ##### 200M cellsize rule ----
@@ -54,21 +61,23 @@ gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FA
     input1 <- sf::st_bbox(input)
 
     # standardize bbox
-    input1[1] <- 2800000+(floor((input1[1]-2800000)/1000)*1000)
-    input1[2] <- 7350000+(floor((input1[2]-7350000)/1000)*1000)
-    input1[3] <- 2800000+(ceiling((input1[3]-2800000)/1000)*1000)
-    input1[4] <- 7350000+(ceiling((input1[4]-7350000)/1000)*1000)
+    input1[1] <- 2800000 + (floor((input1[1] - 2800000) / 1000) * 1000)
+    input1[2] <- 7350000 + (floor((input1[2] - 7350000) / 1000) * 1000)
+    input1[3] <- 2800000 + (ceiling((input1[3] - 2800000) / 1000) * 1000)
+    input1[4] <- 7350000 + (ceiling((input1[4] - 7350000) / 1000) * 1000)
 
     # bbox class to sf class
     input1 <- sf::st_as_sfc(input1)
 
     # make the grid
-    grid1 <- sf::st_make_grid(x = input1,
-                              cellsize = 200,
-                              crs = gridbr_crs,
-                              what = "polygons",
-                              square = TRUE,
-                              flat_topped = FALSE)
+    grid1 <- sf::st_make_grid(
+      x = input1,
+      cellsize = 200,
+      crs = gridbr_crs,
+      what = "polygons",
+      square = TRUE,
+      flat_topped = FALSE
+    )
 
     # create a support dataset, to create id
     grid1c <- sf::st_coordinates(suppressWarnings(sf::st_centroid(grid1)))
@@ -80,13 +89,15 @@ gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FA
     sf::st_geometry(grid1) <- "geom"
 
     # recreate the IBGE id
-    grid1$id <- paste0("200M",
-                       "E",
-                       as.numeric(substr(formatC((2800000+(floor((grid1c[,1]-2800000)/200)*200)), width = 7, format = "d", flag = "0"), start = 1, stop = 5)),
-                       "N",
-                       as.numeric(substr(formatC((7350000+(ceiling((grid1c[,2]-7350000)/200)*200)), width = 8, format = "d", flag = "0"), start = 1, stop = 6)))
+    grid1$id <- paste0(
+      "200M",
+      "E",
+      as.numeric(substr(formatC((2800000 + (floor((grid1c[, 1] - 2800000) / 200) * 200)), width = 7, format = "d", flag = "0"), start = 1, stop = 5)),
+      "N",
+      as.numeric(substr(formatC((7350000 + (ceiling((grid1c[, 2] - 7350000) / 200) * 200)), width = 8, format = "d", flag = "0"), start = 1, stop = 6))
+    )
 
-    grid1 <- grid1[grid1$id %in% gridbr::gridbr_urban,]
+    grid1 <- grid1[grid1$id %in% gridbr::gridbr_urban, ]
 
     ##### PART 2 - 1KM ----
 
@@ -94,21 +105,23 @@ gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FA
     input2 <- sf::st_bbox(input)
 
     # fix bbox to match IBGE grid
-    input2[1] <- 2800000+(floor((input2[1]-2800000)/1000)*1000)
-    input2[2] <- 7350000+(floor((input2[2]-7350000)/1000)*1000)
-    input2[3] <- 2800000+(ceiling((input2[3]-2800000)/1000)*1000)
-    input2[4] <- 7350000+(ceiling((input2[4]-7350000)/1000)*1000)
+    input2[1] <- 2800000 + (floor((input2[1] - 2800000) / 1000) * 1000)
+    input2[2] <- 7350000 + (floor((input2[2] - 7350000) / 1000) * 1000)
+    input2[3] <- 2800000 + (ceiling((input2[3] - 2800000) / 1000) * 1000)
+    input2[4] <- 7350000 + (ceiling((input2[4] - 7350000) / 1000) * 1000)
 
     # bbox class to sf class
     input2 <- sf::st_as_sfc(input2)
 
     # make the grid
-    grid2 <- sf::st_make_grid(x = input2,
-                              cellsize = 1000,
-                              crs = gridbr_crs,
-                              what = "polygons",
-                              square = TRUE,
-                              flat_topped = FALSE)
+    grid2 <- sf::st_make_grid(
+      x = input2,
+      cellsize = 1000,
+      crs = gridbr_crs,
+      what = "polygons",
+      square = TRUE,
+      flat_topped = FALSE
+    )
 
     # grid class to sf (again)
     grid2 <- sf::st_as_sf(grid2)
@@ -118,48 +131,53 @@ gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FA
 
     # get only outside intersection
     inters <- lengths(sf::st_intersects(suppressWarnings(sf::st_centroid(grid2)), suppressWarnings(sf::st_centroid(grid1)))) > 0
-    grid2 <- grid2[!inters,]
+    grid2 <- grid2[!inters, ]
 
     # create a support dataset, to create id
     grid2c <- sf::st_coordinates(suppressWarnings(sf::st_centroid(grid2)))
 
     # recreate the IBGE id
-    grid2$id <- paste0("1KM",
-                       "E",
-                       as.numeric(substr(formatC((2800000+(floor((grid2c[,1]-2800000)/1000)*1000)), width = 7, format = "d", flag = "0"), start = 1, stop = 4)),
-                       "N",
-                       as.numeric(substr(formatC((7350000+(floor((grid2c[,2]-7350000)/1000)*1000)), width = 8, format = "d", flag = "0"), start = 1, stop = 5)))
+    grid2$id <- paste0(
+      "1KM",
+      "E",
+      as.numeric(substr(formatC((2800000 + (floor((grid2c[, 1] - 2800000) / 1000) * 1000)), width = 7, format = "d", flag = "0"), start = 1, stop = 4)),
+      "N",
+      as.numeric(substr(formatC((7350000 + (floor((grid2c[, 2] - 7350000) / 1000) * 1000)), width = 8, format = "d", flag = "0"), start = 1, stop = 5))
+    )
 
     grid <- rbind(grid1, grid2)
-
   } else {
 
     #### KM cellsize rule ----
 
     # create support cell size object (numeric)
-    cellsize2 <- as.numeric(gsub(pattern = "KM",
-                                 replacement = "000",
-                                 x = cellsize))
+    cellsize2 <- as.numeric(gsub(
+      pattern = "KM",
+      replacement = "000",
+      x = cellsize
+    ))
 
     # get bbox of simple features
     input <- sf::st_bbox(input)
 
     # fix bbox to match IBGE grid
-    input[1] <- 2800000+(floor((input[1]-2800000)/cellsize2)*cellsize2)
-    input[2] <- 7350000+(floor((input[2]-7350000)/cellsize2)*cellsize2)
-    input[3] <- 2800000+(ceiling((input[3]-2800000)/cellsize2)*cellsize2)
-    input[4] <- 7350000+(ceiling((input[4]-7350000)/cellsize2)*cellsize2)
+    input[1] <- 2800000 + (floor((input[1] - 2800000) / cellsize2) * cellsize2)
+    input[2] <- 7350000 + (floor((input[2] - 7350000) / cellsize2) * cellsize2)
+    input[3] <- 2800000 + (ceiling((input[3] - 2800000) / cellsize2) * cellsize2)
+    input[4] <- 7350000 + (ceiling((input[4] - 7350000) / cellsize2) * cellsize2)
 
     # bbox class to sf class
     input <- sf::st_as_sfc(input)
 
     # make the grid
-    grid <- sf::st_make_grid(x = input,
-                             cellsize = cellsize2,
-                             crs = gridbr_crs,
-                             what = "polygons",
-                             square = TRUE,
-                             flat_topped = FALSE)
+    grid <- sf::st_make_grid(
+      x = input,
+      cellsize = cellsize2,
+      crs = gridbr_crs,
+      what = "polygons",
+      square = TRUE,
+      flat_topped = FALSE
+    )
 
     # create a support dataset, to create id
     gridc <- sf::st_coordinates(suppressWarnings(sf::st_centroid(grid)))
@@ -172,30 +190,32 @@ gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FA
 
     # recreate IBGE id
     grid$id <- ifelse(rep(cellsize2, times = nrow(grid)) >= 1000,
-                      paste0(cellsize,
-                             "E",
-                             as.numeric(substr(formatC((2800000+(floor((gridc[,1]-2800000)/cellsize2)*cellsize2)), width = 7, format = "d", flag = "0"), start = 1, stop = 4)),
-                             "N",
-                             as.numeric(substr(formatC((7350000+(floor((gridc[,2]-7350000)/cellsize2)*cellsize2)), width = 8, format = "d", flag = "0"), start = 1, stop = 5))),
-                      paste0(cellsize,
-                             "E",
-                             as.numeric(substr(formatC((2800000+(floor((gridc[,1]-2800000)/cellsize2)*cellsize2)), width = 7, format = "d", flag = "0"), start = 1, stop = 5)),
-                             "N",
-                             as.numeric(substr(formatC((7350000+(ceiling((gridc[,2]-7350000)/cellsize2)*cellsize2)), width = 8, format = "d", flag = "0"), start = 1, stop = 6))))
+      paste0(
+        cellsize,
+        "E",
+        as.numeric(substr(formatC((2800000 + (floor((gridc[, 1] - 2800000) / cellsize2) * cellsize2)), width = 7, format = "d", flag = "0"), start = 1, stop = 4)),
+        "N",
+        as.numeric(substr(formatC((7350000 + (floor((gridc[, 2] - 7350000) / cellsize2) * cellsize2)), width = 8, format = "d", flag = "0"), start = 1, stop = 5))
+      ),
+      paste0(
+        cellsize,
+        "E",
+        as.numeric(substr(formatC((2800000 + (floor((gridc[, 1] - 2800000) / cellsize2) * cellsize2)), width = 7, format = "d", flag = "0"), start = 1, stop = 5)),
+        "N",
+        as.numeric(substr(formatC((7350000 + (ceiling((gridc[, 2] - 7350000) / cellsize2) * cellsize2)), width = 8, format = "d", flag = "0"), start = 1, stop = 6))
+      )
+    )
 
-  grid <- rbind(grid)
-
+    grid <- rbind(grid)
   }
 
   ##### census_data parameter ----
   if (census_data == TRUE) {
-
     j <- 1
 
     data <- list()
 
     for (i in cellsizes) {
-
       cs <- any(grepl(pattern = i, x = grid$id))
 
       if (cs == TRUE) {
@@ -211,9 +231,7 @@ gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FA
         data[j] <- mget(paste0("gridbr_", i))
 
         j <- j + 1
-
       }
-
     }
 
     data <- do.call(rbind, data)
@@ -226,7 +244,6 @@ gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FA
     grid$FEM <- ifelse(is.na(grid$FEM), 0, grid$FEM)
     grid$POP <- ifelse(is.na(grid$POP), 0, grid$POP)
     grid$DOM_OCU <- ifelse(is.na(grid$DOM_OCU), 0, grid$DOM_OCU)
-
   }
 
   ##### equal_area parameter ----
@@ -236,5 +253,4 @@ gridbr_download <- function(input, cellsize, census_data = TRUE, equal_area = FA
 
   ##### export final object ----
   return(grid)
-
 }
